a0f5c1d656cd3316b44ee4e82877d797a6858cd9
KSS095 <tmdtn339@gmail.com>
2025-08-30 23:45:13 +0900

    2025_08_30 at home

--- 100_offline/4. Algorithm/31일차 (8.20, mst)/5249_최소신장트리/sol.py
+++ 100_offline/4. Algorithm/31일차 (8.20, mst)/5249_최소신장트리/sol.py
@@ -41,3 +41,58 @@ for tc in range(1, T + 1):
         min_weight_sum += mst_list[2]
 
     print(f'#{tc} {min_weight_sum}')
+
+
+
+
+# import sys
+# sys.stdin = open('sample_input.txt')
+#
+# def find_set(x):
+#     if x == parent[x]:
+#         return parent[x]
+#     parent[x] = find_set(parent[x])
+#     return parent[x]
+#
+# def union(x, y):
+#     root_x = find_set(x)
+#     root_y = find_set(y)
+#
+#     if root_x != root_y:
+#         if rank[root_x] > rank[root_y]:
+#             parent[root_y] = root_x
+#         elif rank[root_y] > rank[root_x]:
+#             parent[root_x] = root_y
+#         else:
+#             parent[root_y] = root_x
+#             rank[root_x] += 1
+#
+#
+# def mst(edges):
+#     mst_list = []
+#     edges.sort(key=lambda x: x[2])
+#
+#     for edge in edges:
+#         x, y, w = edge
+#         if find_set(x) != find_set(y):
+#             union(x, y)
+#             mst_list.append(edge)
+#
+#     return mst_list
+#
+#
+# T = int(input())
+# for tc in range(1, T + 1):
+#     V, E = map(int, input().split())
+#     edges = [list(map(int, input().split())) for _ in range(E)]
+#
+#     parent = [i for i in range(V + 1)]
+#     rank = [0] * (V + 1)
+#     min_weight_sum = 0
+#
+#     mst_lists = mst(edges)
+#
+#     for mst_list in mst_lists:
+#         min_weight_sum += mst_list[2]
+#
+#     print(f'#{tc} {min_weight_sum}')
