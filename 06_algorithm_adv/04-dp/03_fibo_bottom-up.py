6ee3cca57886e0b924db8ee6d4702a5c4803ad27
KSS095 <tmdtn339@gmail.com>
2025-08-31 16:12:22 +0900

    2025_08_31 at home

--- 06_algorithm_adv/04-dp/03_fibo_bottom-up.py
+++ 06_algorithm_adv/04-dp/03_fibo_bottom-up.py
@@ -1,4 +1,6 @@
 def fibo(N):
+    global cnt
+
     if N < 1:
         return N
 
@@ -8,7 +10,11 @@ def fibo(N):
 
     for i in range(2, N + 1):
         dp[i] = dp[i - 1] + dp[i - 2]
+        cnt += 1
     return dp[N]
 
+
+cnt = 0
 result = fibo(10001)
 print(result)   # N번 연산
+print(cnt)
