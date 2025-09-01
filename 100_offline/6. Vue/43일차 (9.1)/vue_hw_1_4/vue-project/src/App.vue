5248d5e3321d98748e1fcf13f4a4922f6235a4a7
KSS095 <tmdtn339@gmail.com>
2025-09-01 17:41:56 +0900

    vue add

---
+++ 100_offline/6. Vue/43일차 (9.1)/vue_hw_1_4/vue-project/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <ColorChanger />
+  </div>
+</template>
+
+<script>
+import ColorChanger from "./components/ColorChanger.vue";
+
+export default {
+  name: "App",
+  components: {
+    ColorChanger,
+  },
+};
+</script>
---
+++ 100_offline/6. Vue/43일차 (9.1)/vue_hw_1_4/vue-project/src/components/ColorChanger.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <!-- 사용자 입력 -->
+    <input v-model="colorClass" placeholder="red / blue / green 입력" />
+
+    <!-- 색상 바뀌는 p 태그 -->
+    <p :class="colorClass">
+      입력창에 올바른 색상 명을 입력하면 글자색이 바껴요.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ColorChanger",
+  data() {
+    return {
+      colorClass: "", // 사용자가 입력한 값 저장
+    };
+  },
+};
+</script>
+
+<style scoped>
+.red {
+  color: red;
+}
+.blue {
+  color: blue;
+}
+.green {
+  color: green;
+}
+</style>
