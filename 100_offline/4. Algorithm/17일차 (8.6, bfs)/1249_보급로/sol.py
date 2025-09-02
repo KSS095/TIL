e41b1999bf06dd0dab247c4e3b082e469d05418b
KSS095 <tmdtn339@gmail.com>
2025-09-02 15:28:03 +0900

    2025_09_02 add

--- 100_offline/4. Algorithm/17일차 (8.6, bfs)/1249_보급로/sol.py
+++ 100_offline/4. Algorithm/17일차 (8.6, bfs)/1249_보급로/sol.py
@@ -1,42 +1,40 @@
 import sys
 sys.stdin = open('input.txt')
 
-from collections import deque
-#     상  하  좌  우
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+import heapq
 
-def supply_route(road, N):
-    queue = deque()
-    queue.append((0, 0))
+#     상  하 좌 우
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
 
-    visited = [[False] * N for _ in range(N)]
-    route_weight = 0
-    min_route_weight =
-    visited[0][0] = True
 
-    while queue:
-        row, col = queue.popleft()
+def move_to_end():
+    heap = []
+    heapq.heappush(heap, (0, 0, 0)) # x, y, dist
+    distances = [[float('inf')] * N for _ in range(N)]  # 모든 지점에서의 거리를 inf 로 저장
 
-        for k in range(4):
-            nx = row + dx[k]
-            ny = col + dy[k]
+    while heap:
+        r, c, dist = heapq.heappop(heap)
+        if distances[r][c] < dist: continue         # 현재 저장되어있는 거리보다 길다면 패스
+        if (r, c) == (N - 1, N - 1): return dist    # 도착점에 도달했다면 계산한 거리 반환
 
-            # 경계 밖으로 나가는지, 방문 했는지 확인
-            if (0 <= nx < 16) and (0 <= ny < 16) and (not visited[nx][ny]):
-                visited[nx][ny] = True
-                queue.append((nx, ny))
-                route_weight += road[nx][ny]
+        for i in range(4):  # 네 방향 탐색
+            nr, nc = r + dr[i], c + dc[i]
 
-    min_route_weight = max(min_route_weight, route_weight)
+            if 0 <= nr < N and 0 <= nc < N: # 벽 탐색
+                next_distance = dist + war_map[nr][nc]  # 다음 장소까지 거리
+
+                if next_distance < distances[nr][nc]:   # 현재 저장되어있는 거리보다 짧다면
+                    distances[nr][nc] = next_distance   # 갱신
+                    heapq.heappush(heap, (nr, nc, next_distance))   # 힙에 삽입
+
+    return distances[N - 1][N - 1]  # 도착점에 저장된 거리 반환
 
 
 T = int(input())
-
-for C in range(1, T + 1):
+for tc in range(1, T + 1):
     N = int(input())
-    supply_road = [list(map(int, input())) for _ in range(N)]
+    war_map = [list(map(int, input())) for _ in range(N)]
 
-    restore_time = supply_route(supply_road, N)
-
-    print(f'#{C} {restore_time}')
+    min_distance = move_to_end()    # 거리 계산
+    print(f'#{tc} {min_distance}')
